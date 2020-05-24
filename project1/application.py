6b1f2b0b4c9acadbb9bb5aeee4d04c7aefdf52b9
Mihir <mihirm5695@gmail.com>
2020-05-24 11:16:46 +0530

    Created Page Not Found Route for Project 1

--- project1/application.py
+++ project1/application.py
@@ -1,10 +1,13 @@
+# importing required stuff
 import os
-
-from flask import Flask, session
+import json
+import requests
+from flask import Flask, session, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+# Init Flask app
 app = Flask(__name__)
 
 # Check for environment variable
@@ -16,6 +19,12 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+with open("../../goodreads_key.json.txt") as json_file:
+    goodreads_json = json.load(json_file)
+
+# print(goodreads_json['key'])
+
+
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
@@ -23,4 +32,13 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
+
+    res = requests.get("https://www.goodreads.com/book/review_counts.json",
+                       params={"key": goodreads_json['key'], "isbns": "9781632168146"})
+    print(res.json())
     return "Project 1: TODO"
+
+
+@app.errorhandler(404)
+def page_not_found():
+    render_template("page_not_found.html"), 404
---
+++ project1/startup.bat
@@ -0,0 +1,5 @@
+set FLASK_APP=application.py
+set FLASK_ENV=development
+set FLASK_DEBUG=1
+set DATABASE_URL=postgres://dqhwkzlubtfthy:5ac85a2822b3b72120fa8e01ae0a36e08bcc37cbca4694e52a39185366199105@ec2-54-247-122-209.eu-west-1.compute.amazonaws.com:5432/d60hivl77cd938
+flask run
